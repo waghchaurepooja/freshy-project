90aeb30630ec8445e577c30eb12a009c55764f9f
waghchaurepooja <97495331+waghchaurepooja@users.noreply.github.com>
2022-05-09 11:17:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Learn } from './components/Learn';
+
+
+function App() {
+  return (
+    <div className="App"> 
+
+<Learn/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Learn.jsx
@@ -0,0 +1,137 @@
+import "./Learn.css";
+
+export const Learn = () => {
+  return (
+    <div>
+      <div className="banner__Image">
+        <img
+          src="https://assets-global.website-files.com/5d03b4e13011831ae4624b37/61f1352892ee6b7e3fd63371_Rectangle.jpg"
+          alt=""
+        />
+      </div>
+
+      <div className="Chaning__Way">
+        <p>Changing the Way You</p>
+        <p>Eat Changes</p>
+        <p>Everything</p>
+        <p>
+          But we know if it's not easy, you won’t do it. And if it doesn’t taste
+          good, you won’t want it. We believe in simple, effortless solutions
+          designed for the unpredictability of real life, that’s why our meals
+          aren’t just ready in minutes, or just delicious, or just nutritious.
+        </p>
+        <p>We’re all 3, always.</p>
+      </div>
+
+      <div className="Less__Sugar">
+        <div>
+          <p>Less Sugar</p>
+          <p>We look at both the amount of sugar used and the source.</p>
+          <p>
+            While the average American is consuming 100 grams of sugar a day,
+            our meals average 8 grams per meal, which means 60%+ reduction every
+            week.
+          </p>
+        </div>
+        <div>
+          <img
+            src="https://assets-global.website-files.com/5d03b4e13011831ae4624b37/61f13539168a0cd057b76e64_22_FoodPhilo_LessSugar_882x428.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="Less__Processed">
+        <div>
+          <p>Less Processed</p>
+          <p>
+            We use real, whole foods and less of the ingredients you can’t
+            pronounce.
+          </p>
+          <p>
+            The American diet is estimated to contain more than 60%
+            ultra-processed foods like ready-to-eat boxed meals, junk-food, and
+            refined starches.
+          </p>
+        </div>
+        <div>
+          <img
+            src="https://assets-global.website-files.com/5d03b4e13011831ae4624b37/61f135436f38a1bcb0509bf3_22_FoodPhilo_LessProcessed_882x428.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="Less__Sugar">
+        <div>
+          <p>More Nutrients</p>
+          <p>
+            We up your nutrient intake with clever swaps, hacks, and
+            well-balanced recipes.
+          </p>
+          <p>
+            Only 10% of Americans get the recommended amount of nutrient-rich
+            veggies every day. That’s why you’ll find veggies in every Freshly®
+            meal.
+          </p>
+        </div>
+        <div>
+          <img
+            src="https://assets-global.website-files.com/5d03b4e13011831ae4624b37/61f1354a094231a1c0053548_22_FoodPhilo_MoreNutrients_882x428.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="Freshly__Five">
+        <div>
+          <p>The Freshly Five: How our</p>
+          <p>fresh prepared meals come</p>
+          <p>to life</p>
+          <p>
+            We take a fresh approach to the food industry by delivering
+            balanced, flavorful, better-for-you meals right to your door.
+            Complex carbs, Nutrient-dense ingredients, Quality proteins, Healthy
+            fats
+          </p>
+
+          <p>Only thoughtful ingredients</p>
+          <p>What we leave out</p>
+          <p>
+            Meals are only as good as the quality of the ingredients that go
+            into them, so you’ll never find artificial flavors, chemical
+            preservatives, or excess sugar in our food. Many of our meals are
+            certified gluten-free and each and every meal excludes 85+ banned
+            ingredients Freshly® says “no” to.
+          </p>
+        </div>
+        <div>
+          <img src="https://assets-global.website-files.com/5d03b4e13011831ae4624b37/5ead4aec85c94f02d4a7667a_content%402x.png" alt="" />
+        </div>
+      </div>
+
+      <div className="NextTopicBox">
+        <div className="NextTopic__logo">
+          <img
+            src="https://assets-global.website-files.com/5d03b4e13011831ae4624b37/5ead4e62b8d39c2f326c766a_leaf.svg"
+            alt=""
+          />
+        </div>
+        <h1>Next Topic: Freshly Vs.</h1>
+
+        <p>Flip to next topic</p>
+      </div>
+
+      <div className="GetStarted__Box">
+        <h1>Our team makes every meal matter</h1>
+        <p>
+          Trust us, it takes a lot of teamwork to make nutritious meals taste
+          this good! Our meals are crafted by a team of chefs, culinary
+          scientists, and nutritionists based on a variety of cuisines and
+          customer feedback.
+        </p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+};
